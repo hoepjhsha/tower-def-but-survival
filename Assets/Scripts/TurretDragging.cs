eaf01b9eb60971f80fc5ed919a3b7e597c1133b6
hoepjhsha <hiepnguyen3624@gmail.com>
2023-10-13 20:47:23 +0700

    addd again

---
+++ Assets/Scripts/TurretDragging.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerDragging : MonoBehaviour
+{
+    public Transform center;
+
+    private Vector3 mouseHitPoint;
+    private Vector3 distanceOffset;
+    [SerializeField]
+    private float radius = 5f;
+
+    [SerializeField]
+    private bool isDragging = false;
+
+    private void OnMouseDown()
+    {
+        mouseHitPoint = getMousePoint();
+        distanceOffset = transform.position - mouseHitPoint;
+        isDragging = true;
+    }
+
+    private void OnMouseUp()
+    {
+        isDragging = false;
+    }
+
+    private void Start()
+    {
+        center = GameObject.Find("Base Tower").transform;
+    }
+
+    private void Update()
+    {
+        if (isDragging)
+        {
+            Vector3 movPosition = getMousePoint() + distanceOffset;
+            Vector3 checkCirclePosiion = movPosition - center.position;
+            Vector3 newPosition = center.position + checkCirclePosiion.normalized * radius;
+            newPosition.y = transform.position.y;
+
+            transform.position = newPosition;
+        }
+    }
+
+    Vector3 getMousePoint()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero); // Bề mặt ngang với Vector3.up là hướng pháp tuyến
+
+        float rayDistance;
+        if (groundPlane.Raycast(ray, out rayDistance))
+        {
+            Vector3 hitPoint = ray.GetPoint(rayDistance);
+            return hitPoint;
+        }
+
+        return Vector3.zero;
+    }
+}
---
+++ Assets/Scripts/TurretShooting.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+
+public class TurretShooting : MonoBehaviour
+{
+    [SerializeField]
+    private int attackDamage = 50;
+    [SerializeField]
+    private float attackRange = 20f;
+    [SerializeField]
+    private float attackCooldown = 1f;
+    private float currentCooldown = 0f;
+
+    public GameObject bulletPrefab;
+
+    private void Update()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, attackRange);
+        foreach (Collider collider in colliders)
+        {
+            if (collider.CompareTag("Enemy"))
+            {
+                Attack(collider.gameObject, attackDamage);
+                break;
+            }
+        }
+        //Attack(FindNearestEnemy());
+    }
+
+    GameObject FindNearestEnemy()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, attackRange);
+        GameObject nearestEnemy = null;
+        float nearestDistance = Mathf.Infinity;
+
+        foreach (Collider collider in colliders)
+        {
+            if (collider.CompareTag("Enemy"))
+            {
+                float distance = Vector3.Distance(transform.position, collider.transform.position);
+                if (distance < nearestDistance)
+                {
+                    nearestDistance = distance;
+                    nearestEnemy = collider.gameObject;
+                }
+            }
+        }
+
+        return nearestEnemy;
+    }
+
+    void Attack(GameObject target, int attackDamage)
+    {
+        if (currentCooldown <= 0f)
+        {
+            Vector3 bulletPosition = transform.position;
+            bulletPosition.y = 1f;
+            GameObject bullet = Instantiate(bulletPrefab, bulletPosition, Quaternion.identity);
+
+            BulletScript bulletScript = bullet.GetComponent<BulletScript>();
+            if (bulletScript != null)
+            {
+                bulletScript.SetTarget(target, attackDamage);
+            }
+            currentCooldown = attackCooldown;
+        }
+        else
+        {
+            currentCooldown -= Time.deltaTime;
+        }
+    }
+
+    //void OnDrawGizmos()
+    //{
+    //    Gizmos.color = Color.red;
+    //    Gizmos.DrawWireSphere(transform.position, attackRange);
+    //}
+}
